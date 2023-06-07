8a5b98b5b48fc65e4b302e1d8a5dc975c631c388
Godspower Similoluwa <gpsimi01@gmail.com>
2023-06-07 22:02:48 +0100

    update

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+//mobile navigation
+
+const openIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".fa-times-circle");
+const mobileBG = document.querySelector(".mobile-bg");
+const mobileNav = document.querySelector(".mobile-nav");
+
+openIcon.addEventListener ("click", () => {
+    mobileBG.classList.toggle("display-none");
+    mobileNav.classList.remove("slide-out");
+    mobileNav.classList.toggle("slide-in");
+    openIcon.classList.toggle("display-none");
+});
+
+closeIcon.addEventListener("click", () => {
+    setTimeout(() => {
+        mobileBG.classList.toggle("display-none");
+        openIcon.classList.toggle("display-none");
+    }, 500)
+    mobileNav.classList.toggle("slide-in");
+    mobileNav.classList.toggle("slide-out");
+});
